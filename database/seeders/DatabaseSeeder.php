d0b8f42277d3d0d3d175b5f1cdde2b3bcf1ba79f
Joseph Jett Abela <jetpackzabela12@gmail.com>
2026-06-25 14:53:13 +0800

    feat applying package add ons on database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,7 +32,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            PaymentOptionSeeder::class
+            PaymentOptionSeeder::class,
+            PackageAddOnSeeder::class,
         ]);
     }
 }
