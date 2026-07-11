f8b45e06c057f490089e159fc6f9d2249956191e
Joseph Jett Abela <jetpackzabela12@gmail.com>
2026-07-11 10:32:34 +0800

    fix booking migration error

--- database/migrations/2026_06_16_230623_create_bookings_table.php
+++ database/migrations/2026_06_16_230623_create_bookings_table.php
@@ -41,6 +41,10 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::disableForeignKeyConstraints();
+
         Schema::dropIfExists('bookings');
+        
+        Schema::enableForeignKeyConstraints();
     }
 };
